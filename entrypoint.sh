2663dbd788e4d35b1f83bcc03ca89d7fe4fb625e
Haocheng Hu <hc@bevycommerce.com>
2025-11-06 12:14:31 -0500

    increase worker count

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 python3 manage.py createhorillauser --first_name bevy --last_name admin --username bevyadmin --password bevyadmin --email hr@bevycommerce.com --phone 1234567890
-gunicorn --bind 0.0.0.0:8000 horilla.wsgi:application
+gunicorn --workers 4 --bind 0.0.0.0:8000 horilla.wsgi:application
